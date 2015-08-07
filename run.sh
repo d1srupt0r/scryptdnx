aa8f9f3d35cd8b59d07b7602c18bfcca3ec6ba96
d1srupt0r <disruptorq2k@yahoo.com>
2015-08-07 12:41:47 -0700

    build, log then execute

--- run.sh
+++ run.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
+# BUILD
+xbuild /nologo /verbosity:quiet
+
 # LOG
 gendarme --quiet --log todo.log --html bin/gendarme.html  bin/scryptdnx.exe
 
-# Execute
+# RUN
 mono bin/scryptdnx.exe $@
